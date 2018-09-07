2bccd8d5f837b76a3d7c7843591d7fb87a4e37bb
NicolaasArys <43039357+NicolaasArys@users.noreply.github.com>
2018-09-07 15:14:34 +0200

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+$serverAdress = "localhost";
+$userName = "root";
+$password = "";
+$tableName = "helpdesk"; // Verander dit naar jouw tabel naam
+
+$conn = mysqli_connect($serverAdress, $userName, $password, $tableName);
+?>
