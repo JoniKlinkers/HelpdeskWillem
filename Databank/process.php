47a5990dac251f8e6bedded25fb92dba9c19e280
JoniKlinkers <43039356+JoniKlinkers@users.noreply.github.com>
2018-09-13 16:01:46 +0200

    seshvariabele
    seshvariabele

--- Databank/process.php
+++ Databank/process.php
@@ -3,7 +3,7 @@
     include "connect.php";
 
     $GebruikerEmail = $_POST['GebruikerEmail'];
-    $password = $_POST['password'];
+    $password = $_POST['GebruikerWW'];
     if($_POST['GebruikerEmail'] == null){
         echo "Fout ingevuld probeer opnieuw.";
         echo '</br> <a href="index.php">back</a>';
@@ -12,9 +12,11 @@
  
     if($resultaat = $conn->query($sql)){
         $row = $resultaat->fetch_assoc();
-        if(password_verify($password, $row['password'])){
+        if(password_verify($password, $row['GebruikerWW'])){
             $_SESSION['GebruikerEmail'] = $GebruikerEmail;
             $_SESSION['GebruikerType'] = $row['GebruikerType'];
+            $_SESSION['GebruikerWW'] = $row['GebruikerWW'];
+            $_SESSION['GebruikerId'] = $row['GebruikerId'];
             header('location: ../index.php');
         }else {
         echo "Fout ingevuld probeer opnieuw.";
