1fcf78f146dc5f074bfc14841ce0d06c551c6902
jaco360 <43039409+jaco360@users.noreply.github.com>
2018-09-13 14:20:57 +0200

    php nog is

--- Website/layout/login.php
+++ Website/layout/login.php
@@ -22,8 +22,8 @@
                    <h4>MANGO'S</h4>
                </div>
                <div class="menu">
-                   <a href="login.html"><button type="submit" value="Aanmelden" class="button">Aanmelden</button></a>
-                  <a href="registeren.html"><button type="submit" value="Aanmelden" class="button">Registeren</button></a>
+                   <a href="login.php"><button type="submit" value="Aanmelden" class="button">Aanmelden</button></a>
+                  <a href="registeren.php"><button type="submit" value="Aanmelden" class="button">Registeren</button></a>
                </div>
            </div>
             <div class="header-search">
