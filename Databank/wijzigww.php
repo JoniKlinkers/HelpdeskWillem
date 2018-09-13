4fb3aa192acf7ff34226b5c918a3a011ae99cef8
JoniKlinkers <43039356+JoniKlinkers@users.noreply.github.com>
2018-09-13 16:07:24 +0200

    seshvariabele
    seshvariabele

--- Databank/wijzigww.php
+++ Databank/wijzigww.php
@@ -1,13 +1,13 @@
 <?php
 include "connect.php";
 session_start();
-$sql = "SELECT GebruikerWW FROM TblGebruikers WHERE GebruikerId ='1'";
+$sql = "SELECT GebruikerWW FROM TblGebruikers WHERE GebruikerId ='".$_SESSION['GebruikerId']."'";
 $ww = $conn->query($sql);
 if(isset($_POST['knop'])){
     while($row = $ww->fetch_assoc()) {
         if(password_verify($_POST['wachtwoord'], $row['GebruikerWW'])){
             if($_POST['checkww'] == $_POST['nieuwww']){
-                $sql = "UPDATE TblGebruikers SET GebruikerWW ='".$_POST['nieuwww']."' WHERE GebruikerId ='1'";
+                $sql = "UPDATE TblGebruikers SET GebruikerWW ='".$_POST['nieuwww']."' WHERE GebruikerId ='".$_SESSION['GebruikerId']."'";
                 if(mysqli_query($conn, $sql)){
                     echo "Je hebt je wachtwoord succesvol gewijzigd.";
                 }else{
