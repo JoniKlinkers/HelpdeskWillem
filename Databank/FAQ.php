56ace1f537eb480ccfa26b511ff78c9ad1554d73
Jonanien <jonathanvelkeneers@hotmail.com>
2018-09-13 14:54:27 +0200

    joeFAQ

--- Databank/FAQ.php
+++ Databank/FAQ.php
@@ -1,16 +1,16 @@
 <?php
 	include 'connect.php';
-    $sql = "select * from tblfaq";
+    $vraag = 'blabal'
+    $sql = "select * from tblfaq where FAQVraag is ".$vraag."";
     $resultaat = $conn->query($sql);
     echo"<table>";
     echo"<tr>";
-    echo"<td>Productnummer</td>";
-    echo"<td>Productnaam</td>";
-    echo"<td>Prijs</td>";
-    echo"<td>Stock</td>";
+    echo"<td>VraagID</td>";
+    echo"<td>Vraag</td>";
+    echo"<td>Antwoord</td>";
     echo"</tr>";
     while ($row = $resultaat->fetch_assoc()) {
-        echo "<tr><td>". $row['Productnummer'] ."</td><td>". $row['Productnaam'] ."</td><td>€". $row['Prijs'] ."</td><td>". $row['Stock'] ."stuks</td><td><a href=wissenProduct.php?tewissen=". $row['Productnummer'].">Wissen</a></td><td><a href=wijzigProduct.php?teveranderen=". $row['Productnummer'].">Wijzigen</a></td></tr>";
+        echo "<tr><td>". $row['FAQId'] ."</td><td>". $row['FAQVraag'] ."</td><td>". $row['FAQAntwoord'] ."</td></tr>";
     }
     echo "<table>";
     print "<a href=toevoegenProduct.php>Voeg een record toe</a>";
