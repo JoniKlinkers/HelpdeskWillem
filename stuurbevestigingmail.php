32d743a285e05327438f17e8e72dbd69c54fa9f3
jaco360 <43039409+jaco360@users.noreply.github.com>
2018-09-14 16:18:03 +0200

    hey xxxx

--- stuurbevestigingmail.php
+++ stuurbevestigingmail.php
@@ -2,7 +2,7 @@
 $verificatie=0;
 $hashpassword = password_hash($verificatie, PASSWORD_DEFAULT);
 require("PHPMailer/class.phpmailer.php");
-$bericht ='Nieuwe inschrijving van: helpdesk klik <a>hier</a href="bevestigingpagina.php"> om u account te bevestigen '.$hashpassword;
+$bericht ='Nieuwe inschrijving van: helpdesk vul deze link in op onze site om in te loggen '.$hashpassword;
  
 $mail = new PHPMailer();
 $mail->IsSMTP();
