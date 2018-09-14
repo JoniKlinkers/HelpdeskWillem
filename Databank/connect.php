76b87504be29b28286d00a880e893e9b0469742b
Jonanien <jonathanvelkeneers@hotmail.com>
2018-09-14 15:22:54 +0200

    023

--- Databank/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$serverAdress = "localhost";
-$userName = "root";
-$password = "";
-$tableName = "helpdesk"; // Verander dit naar jouw tabel naam
-
-$conn = mysqli_connect($serverAdress, $userName, $password, $tableName);
-?>
