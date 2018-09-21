1c3d90bcda819128bb126d6e92914f6fa668ad3e
JoniKlinkers <43039356+JoniKlinkers@users.noreply.github.com>
2018-09-21 13:53:07 +0200

    Update profiel.php

--- profiel.php
+++ profiel.php
@@ -2,59 +2,83 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>Helpdesk</title>
+  <title>Profielpagina</title>
   <meta name="description" content="">
   <meta name="author" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
 <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-  <link rel="stylesheet" href="../css/normalize.css">
-  <link rel="stylesheet" href="../css/style.css">
-  <link rel="stylesheet" href="../css/skeleton.css">
-  <link rel="stylesheet" href="../css/skeleton.css">
-  <link rel="icon" type="image/png" href="../images/favicon.png">
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/skeleton.css">
+  <link rel="icon" type="image/png" href="images/favicon.png">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 </head>
 <body>
     <div class="container">
-        <div class="header">
+        <div class="header-small">
            <div class="header-menu">
                <div class="logo">
                    <h4>MANGO'S</h4>
                </div>
                <div class="menu">
-                  <a href="login.php"><button type="submit" value="Aanmelden" class="button">Aanmelden</button></a>
-                  <a href="registeren.php"><button type="submit" value="Aanmelden" class="button">Registeren</button></a>
+                 <li><a href="">Home</a></li>
+                 <li><a href="">Mijn tickets</a></li>
+                 <li><a href="">FAQ</a></li>
                </div>
            </div>
-            <div class="header-search">
-               <h3 class="header-search--title">Zoek een andere vraag</h3>
-                <form method="post" action="FAQ-overzicht.php">
-                    <input type="text" placeholder="Begin je vraag te zoeken ..." name="zoekterm">
-                    <input type="submit" value="Zoeken" class="button-blue">
-                </form>
-            </div>
         </div>
-        <section class="tabel">
-        	<div class="accordion">
-        		<?php
-					include "connect.php";
-					session_start();
-					$sql = "SELECT * FROM TblGebruikers WHERE GebruikerId =".$_SESSION['GebruikerId']."";
-					$result = $conn->query($sql);
-					echo "<table>";
-					while($row = $result->fetch_assoc()) {
-					    echo "<tr><td>Naam: </td><td>".$row['GebruikerNaam']."</td></tr><tr><td>E-mail: </td><td>".$row['GebruikerEmail']."</td></tr>";
-					    echo "<tr><td>Type gebruiker: </td><td>".$row['GebruikerType']."</td></tr><tr><td>Aantal tickets: </td><td>".$row['GebruikerPrestatie']."</td></tr>";
-					}
-					echo "</table>";
-				?>
-        	</div>
-        </section>
     </div>
+    <h1>ProfielPagina</h1>
+    <section class="profiel">
+        <?php
+        include "connect.php";
+            session_start();
+            $sql = "SELECT * FROM TblGebruikers WHERE GebruikerId ='".$_SESSION['GebruikerId']."'";
+            $profiel = $conn->query($sql);
+            while($row = $profiel->fetch_assoc()){
+            echo "<h2>".$row['GebruikerNaam']."</h2>";
+          ?>
+      <div class="row">
+        <div class="six columns">
+          <div class="profiel-foto">
+            <img class="u-max-full-width" src="https://www.db-m.nl/wp-content/uploads/2014/10/geenfoto-1024x683.jpg">
+            <form class="wijzigprofiel" action='profielaanpassen.php' method='post'>
+                <input type='submit' value='Gebruikergegevens Aanpassen' name='knop'>
+            </form>
+          </div>
+        </div>
+        <div class="six columns">
+          <div class="profiel-info">
+            <ul>
+              <li id="bold">Naam</li>
+                <?php
+                echo "<li>".$row['GebruikerNaam']."</li>";
+                ?>
+                <li id="bold">E-mail</li>
+                <?php
+                echo "<li>".$row['GebruikerEmail']."</li>";
+                ?>
+                <li id="bold">GebruikerType</li>
+                <?php
+                echo "<li>".$row['GebruikerType']."</li>";
+                ?>
+                <li id="bold">Aantal tickets</li>
+                <?php
+                echo "<li>".$row['GebruikerPrestatie']."</li>";
+                ?>
+                <li id="bold">Online</li>
+                <li id="green">Aanwezig</li>
+            </ul>
+              <?php 
+            }
+              ?>
+          </div>
+        </div>
+      </div>
+    </section>
     <footer>
         Copyright Mango's 2018. All right reserved.
     </footer>
-    <script src="../js/script.js"></script>
 </body>
 </html>
